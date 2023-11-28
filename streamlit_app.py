9357432079effaf6b592224baec31ac5772fda78
sanjitupadhya <152272643+sanjitupadhya@users.noreply.github.com>
2023-11-28 16:02:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,4 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('kale, Spinach & Rocket Smoothie')
 streamlit.text("Hard-Boiled Free-Range Egg')
+               
