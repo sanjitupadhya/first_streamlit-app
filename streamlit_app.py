7a5dee932d4d9a774e5832778d2ecb2aa8893374
sanjitupadhya <152272643+sanjitupadhya@users.noreply.github.com>
2023-11-28 16:03:25 +0530

    Update streamlit_app.py
    error " resolved

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ streamlit.title('Hello')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('kale, Spinach & Rocket Smoothie')
-streamlit.text("Hard-Boiled Free-Range Egg')
+streamlit.text('Hard-Boiled Free-Range Egg')
                
