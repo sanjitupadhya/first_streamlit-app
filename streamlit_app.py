9e29a902df5e19e38b6bc91e64f26dd04523199a
sanjitupadhya <152272643+sanjitupadhya@users.noreply.github.com>
2023-11-28 17:23:08 +0530

    Update streamlit_app.py
    syntax error

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,6 @@ streamlit.dataframe(my_fruit_list)
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # using multi select o pick the desired fruits for the smoothie
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index,['Avocado','Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
