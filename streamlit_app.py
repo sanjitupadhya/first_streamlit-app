46a42eef17b793dd3dab381bc4384b4a9187042a
sanjitupadhya <152272643+sanjitupadhya@users.noreply.github.com>
2023-11-28 17:07:58 +0530

    Update streamlit_app.py
    added index

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,9 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 
+#option of fruit by name
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
 # using multi select o pick the desired fruits for the smoothie
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
